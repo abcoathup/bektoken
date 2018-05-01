7460186720583a29b98b3a68609e4c727dc9bf03
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2018-05-01 14:15:42 +1000

    Added base unit and reordered contracts

--- contracts/BekToken.sol
+++ contracts/BekToken.sol
@@ -1,15 +1,16 @@
-pragma solidity ^0.4.23;
+pragma solidity 0.4.23;
 
-import "openzeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
 
 /**
  * @title BekToken
  * @dev ERC20 Token
  */
-contract BekToken is CappedToken, DetailedERC20 {
+contract BekToken is DetailedERC20, CappedToken {
+  string public baseUnit = "bee";
   uint8 constant _decimals = 18;
 
-  constructor() CappedToken(100 * 10**9 * 10**uint256(_decimals)) DetailedERC20("BekToken", "BEK", _decimals) public {}
+  constructor() DetailedERC20("BekToken", "BEK", _decimals) CappedToken(100 * 10**9 * 10**uint256(_decimals)) public {}
 
 }
